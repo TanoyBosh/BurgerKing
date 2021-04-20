681ce1acbb80ed1f25c166218844a2e0561b534c
Tanoy Bosh <tanoybosh@gmail.com>
2021-04-20 22:49:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+}
+
+function max(max){
+    var x = document.querySelectorAll(".box");
+    if(x==2){
+        alert("two");
+    }
+}
+max(max);
